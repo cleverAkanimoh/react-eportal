8c404b5f0287cfcf5a3be74c99bef917c18c7690
cleverAkanimoh <crushclever1@gmail.com>
2023-02-06 11:21:23 +0100

    added 16 files

---
+++ src/Components/MiddleSection.jsx
@@ -0,0 +1,13 @@
+// import { VscSearch, VscBell } from "react-icons/vsc"
+
+export default function () {
+  return (
+    <article id="middle__article">
+      <div id="hello__div">
+        <h2>hello, nancy</h2>
+        <VscSearch />
+        <VscBell />
+      </div>
+    </article>
+  )
+}
---
+++ src/Components/Student.jsx
@@ -0,0 +1,13 @@
+import SideBar from "./SideBar";
+import MiddleSection from "./MiddleSection";
+import Aside from "./Aside";
+
+export default function () {
+    return (
+        <section id="student__section">
+            <SideBar />
+            <MiddleSection />
+            <Aside />
+        </section>
+    )
+}
