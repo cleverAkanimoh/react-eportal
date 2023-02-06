1d45539515780c1df104ff51b3c079171d0b29d9
cleverAkanimoh <crushclever1@gmail.com>
2023-02-06 12:59:40 +0100

    modified MiddleSection.jsx in Components

--- src/Components/MiddleSection.jsx
+++ src/Components/MiddleSection.jsx
@@ -1,12 +1,12 @@
-// import { VscSearch, VscBell } from "react-icons/vsc"
+import { VscSearch, VscBell } from "react-icons/vsc"
 
 export default function () {
   return (
     <article id="middle__article">
       <div id="hello__div">
         <h2>hello, nancy</h2>
-        {/* <VscSearch />
-        <VscBell /> */}
+        <VscSearch />
+        <VscBell />
       </div>
     </article>
   )
